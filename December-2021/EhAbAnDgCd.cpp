e9845274d002f5741fa8ce5224237310abb7c3a0
shucshin <robinucscs@gmail.com>
2021-12-02 12:04:25 -0600

    Update Dec02

---
+++ December-2021/EhAbAnDgCd.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << n-1 << " " << 1 << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++) {
+        solve();
+    }
+    return 0;
+}
+
--- December-2021/README.md
+++ December-2021/README.md
@@ -1,6 +1,7 @@
-# Problems Solved :: 1
+# Problems Solved :: 2
 December 2021
 
 CodeForces
 -----------------
 1.[Three Pairwise Maximums](https://codeforces.com/problemset/problem/1385/A)
+1.[EhAb AnD gCd](https://codeforces.com/problemset/problem/1325/A)
