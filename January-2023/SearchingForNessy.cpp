79a1017b611d17b26ed5a48c701a79c275ca088d
shucshin <robinucscs@gmail.com>
2023-01-05 00:13:18 -0600

    Update Jan05

---
+++ January-2023/SearchingForNessy.cpp
@@ -0,0 +1,12 @@
+// 11044
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t, n, m; cin >> t;
+    while(t--) {
+        cin >> n >> m;
+        cout << (n/3)*(m/3) << endl;
+    }
+    return 0;
+}
